e7cdc860c7352f480575f61f39a01c9f319daef4
Gianmauro Cuccuru <gmauro@gmail.com>
2019-12-04 22:21:17 +0100

    add a dict to manage volumes

--- ensure_enough.py
+++ ensure_enough.py
@@ -161,8 +161,7 @@ class StateManagement:
 
         return custom_userdata
 
-
-    def launch_server(self, name, flavor, resource_identifier, group, is_training=False, cgroups=False):
+    def launch_server(self, name, flavor, group, is_training=False, cgroups=False):
         """
         Launch a server with a given name + flavor.
 
@@ -206,14 +205,60 @@ class StateManagement:
         # Wait for this server to become 'ACTIVE'
         return self.wait_for_state(name, 'ACTIVE', escape_states=['ERROR'])
 
-    def launch_server_volume(self, name, flavor, resource_identifier, group, is_training=False, cgroups=False):
+    # def launch_server_volume(self, name, flavor, resource_identifier, group, is_training=False, cgroups=False):
+    #     """
+    #     Launch a server with a given name + flavor.
+    #
+    #     :returns: The launched server
+    #     :rtype: novaclient.v2.servers.Server
+    #     """
+    #     if DRY_RUN: return {'Status': 'OK (fake)'}
+    #
+    #     logging.info("launching %s (%s) with volume", name, flavor)
+    #     # If it's a compute-something, then we just tag as compute, per current
+    #     # sorting hat expectations.
+    #     custom_userdata = self.template_config(group, is_training=is_training, cgroups=cgroups)
+    #
+    #     f = tempfile.NamedTemporaryFile(prefix='ensure-enough.', delete=False)
+    #     f.write(custom_userdata.encode())
+    #     f.close()
+    #
+    #     args = [
+    #         'boot',
+    #         '--image', self.current_image_name,
+    #         '--flavor', flavor,
+    #         '--key-name', self.config['sshkey'],
+    #         '--availability-zone', 'nova',
+    #         '--nic', 'net-id=%s' % self.config['network_id'],
+    #         '--user-data', f.name,
+    #         '--block-device', 'source=blank,dest=volume,size=100,shutdown=remove',
+    #     ]
+    #
+    #     args.append('--security-groups')
+    #     args.append(','.join(self.config['secgroups']))
+    #
+    #     args.append(name)
+    #
+    #     self.os_command(args, cmd='nova', is_json=False)
+    #
+    #     try:
+    #         os.unlink(f.name)
+    #     except:
+    #         pass
+    #
+    #     # Wait for this server to become 'ACTIVE'
+    #     return self.wait_for_state(name, 'ACTIVE', escape_states=['ERROR'])
+
+    def launch_server_volume(self, name, flavor, group, is_training=False, cgroups=False,
+                             vol_size=12, vol_boot=False):
         """
         Launch a server with a given name + flavor.
 
         :returns: The launched server
         :rtype: novaclient.v2.servers.Server
         """
-        if DRY_RUN: return {'Status': 'OK (fake)'}
+        if DRY_RUN:
+            return {'Status': 'OK (fake)'}
 
         logging.info("launching %s (%s) with volume", name, flavor)
         # If it's a compute-something, then we just tag as compute, per current
@@ -226,60 +271,21 @@ class StateManagement:
 
         args = [
             'boot',
-            '--image', self.current_image_name,
             '--flavor', flavor,
             '--key-name', self.config['sshkey'],
             '--availability-zone', 'nova',
             '--nic', 'net-id=%s' % self.config['network_id'],
             '--user-data', f.name,
-            '--block-device', 'source=blank,dest=volume,size=100,shutdown=remove',
+            '--security-groups', ','.join(self.config['secgroups']),
         ]
-
-        args.append('--security-groups')
-        args.append(','.join(self.config['secgroups']))
-
-        args.append(name)
-
-        self.os_command(args, cmd='nova', is_json=False)
-
-        try:
-            os.unlink(f.name)
-        except:
-            pass
-
-        # Wait for this server to become 'ACTIVE'
-        return self.wait_for_state(name, 'ACTIVE', escape_states=['ERROR'])
-
-    def launch_server_boot_volume(self, name, flavor, resource_identifier, group, is_training=False, cgroups=False):
-        """
-        Launch a server with a given name + flavor.
-
-        :returns: The launched server
-        :rtype: novaclient.v2.servers.Server
-        """
-        if DRY_RUN: return {'Status': 'OK (fake)'}
-
-        logging.info("launching %s (%s) with volume", name, flavor)
-        # If it's a compute-something, then we just tag as compute, per current
-        # sorting hat expectations.
-        custom_userdata = self.template_config(group, is_training=is_training, cgroups=cgroups)
-
-        f = tempfile.NamedTemporaryFile(prefix='ensure-enough.', delete=False)
-        f.write(custom_userdata.encode())
-        f.close()
-
-        args = [
-            'boot',
-            '--flavor', flavor,
-            '--key-name', self.config['sshkey'],
-            '--availability-zone', 'nova',
-            '--nic', 'net-id=%s' % self.config['network_id'],
-            '--user-data', f.name,
-            '--block-device', 'source=image,id={},dest=volume,size=100,bootindex=0,shutdown=remove'.format(self.config['image_id']),
-        ]
-
-        args.append('--security-groups')
-        args.append(','.join(self.config['secgroups']))
+        if vol_boot:
+            args.append('--block-device')
+            args.append('source=image,id={},dest=volume,size={}},bootindex=0,shutdown=remove'.format(self.config['image_id'], vol_size))
+        else:
+            args.append('--image')
+            args.append(self.current_image_name)
+            args.append('--block-device')
+            args.append('source=blank,dest=volume,size={},shutdown=remove'.format(vol_size))
 
         args.append(name)
 
@@ -366,7 +372,7 @@ class StateManagement:
                 break
             time.sleep(10)
 
-    def top_up(self, desired_instances, prefix, resource_identifier, flavor, group, boot_volumes=False, volumes=False,
+    def top_up(self, desired_instances, prefix, flavor, group, volume=False, volume_args=None,
                cgroups=False):
         """
         :param int desired_instances: Number of instances of this type to launch
@@ -392,7 +398,6 @@ class StateManagement:
             args = (
                 self.non_conflicting_name(prefix, all_servers),
                 flavor,
-                resource_identifier,
                 group,
             )
 
@@ -401,10 +406,10 @@ class StateManagement:
                 'cgroups': cgroups,
             }
 
-            if volumes:
+            if volume:
+                kwargs['vol_size'] = volume_args['size']
+                kwargs['vol_boot'] = volume_args['boot']
                 server = self.launch_server_volume(*args, **kwargs)
-            elif boot_volumes:
-                server = self.launch_server_boot_volume(*args, **kwargs)
             else:
                 server = self.launch_server(*args, **kwargs)
 
@@ -490,20 +495,21 @@ class StateManagement:
                 else:
                     self.brutally_terminate(server)
 
+                volume = True if 'volume' in resource else False
                 # With that done, 'top up' to the correct number of VMs.
-                self.top_up(desired_instances, prefix, resource_identifier, flavor,
+                self.top_up(desired_instances, prefix, flavor,
                             resource.get('group', resource_identifier),
-                            boot_volumes=resource.get('boot_volumes', False),
-                            volumes=resource.get('volumes', False),
+                            volume=True if 'volume' in resource else False,
+                            volume_args=resource.get('volume', None),
                             cgroups=resource.get('cgroups', False))
 
             # Now that we've removed all that we need to remove, again, try to top-up
             # to make sure we're OK. (Also important in case we had no servers already
             # running.)
-            self.top_up(desired_instances, prefix, resource_identifier, flavor,
+            self.top_up(desired_instances, prefix, flavor,
                         resource.get('group', resource_identifier),
-                        boot_volumes=resource.get('boot_volumes', False),
-                        volumes=resource.get('volumes', False),
+                        volume=True if 'volume' in resource else False,
+                        volume_args=resource.get('volume', None),
                         cgroups=resource.get('cgroups', False))
 
 
